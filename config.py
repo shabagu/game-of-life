af390f9a7e885342d29609bb3d790f2b20159d22
shabagu <shabagu@yandex.ru>
2022-10-28 02:20:05 +0300

    cell deleting on pause

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+# Стандартная цветовая палитра
 COLOR_BG = (10, 10, 10)
 COLOR_GRID = (40, 40, 40)
 COLOR_DYING_CELL = (170, 170, 170)
 COLOR_ALIVE_CELL = (255, 255, 255)
 
-CELL_SIZE = 10
+
+
+# Размер клеток и экрана
+CELL_SIZE = 12
 SCREEN_WIDTH = CELL_SIZE * 80
 SCREEN_HEIGHT = CELL_SIZE * 60
